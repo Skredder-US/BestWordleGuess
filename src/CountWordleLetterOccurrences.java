5fcbfbc92883d58b494ff11f002039ef85f995bb
Taylor Juve <taylor_juve@outlook.com>
2022-03-28 19:29:17 -0700

    "Sores"! is the most coincident word with 9983 coincident letters. However, more work is to be done as this algorithm only considers green (right location) matches. Yellow (right letter, wrong location) matches will be worked in next.

--- src/CountWordleLetterOccurrences.java
+++ src/CountWordleLetterOccurrences.java
@@ -6,42 +6,61 @@ import java.util.Scanner;
  * The Wordle words list has 10663 words! 
  * 
  * Counts occurrences of each letter at each of the five letter positions 
- * of all Wordle words. 
+ * of all Wordle words then finds which word has the most common letters 
+ * for each position
  * 
- * 		eg. : hello has 1 h at 0th index (continue for all words in list) 
+ * 		eg. : SARES are the most common letters for each index (position) but
+ * 			  is not a word. Maybe "Safes" is the word with the most common 
+ * 			  letters in each index. 
  * 
- * Results: SARES are most common letters for each index.
+ * results: "Sores"! (I could of guessed that!) with 9983 coincident letters
  * 
- * next we find the best matching word (a real word, not "SARES")
+ * Notes: i realized this is only for green (perfect match) letters, not letters
+ * that are yellow (in the word, not in this location.) will do next.
  */
 public class CountWordleLetterOccurrences {
 
 	public static void main(String[] args) throws FileNotFoundException {
 		// Iterate through the list and count each letter in each index
-		Scanner listScan = new Scanner(
-				new File("src/Wordle Word List.txt"));
+		Scanner listScan = new Scanner(new File("src/Wordle Word List.txt"));
 		
 		int[][] letterOccurrenceCounts = new int[5][26]; // 5 indices, 26 letters
 		while (listScan.hasNext()) {
 			String curWord = listScan.next();
 			
-			for (int i = 0; i < 5; i++) { // all words are 5 letters
+			for (int i = 0; i < curWord.length(); i++) { 
 				letterOccurrenceCounts[i][curWord.charAt(i) - 'a']++;
 			}
 		}
 		
-		// Print results
-		for (char c = 'A'; c <= 'Z'; c++) {
-			System.out.print(c + "\t");
-		}
-		System.out.println();
+		// Reiterate through the list and find the word with the most common 
+		// letters for each position
 		
-		for (int i = 0; i < letterOccurrenceCounts.length; i++) {
-			for (int j = 0; j < letterOccurrenceCounts[i].length; j++) {
-				System.out.print(letterOccurrenceCounts[i][j] + "\t");
+		// quick redo of File and rescan b/c easy
+		listScan = new Scanner(new File("src/Wordle Word List.txt"));
+		
+		String curBestWord = "";
+		int curOccurrenceCountMax = 0;
+		while (listScan.hasNext()) {
+			String curWord = listScan.next();
+			
+			// sum the occurrence counts for the letters in this word at each
+			// index
+			int curOccurrenceCountSum = 0;
+			for (int i = 0; i < curWord.length(); i++) {
+				curOccurrenceCountSum +=
+						letterOccurrenceCounts[i][curWord.charAt(i) - 'a'];
 			}
-			System.out.println();
+			
+			// set this as best word if occurrence counts is largest.
+			if (curOccurrenceCountSum > curOccurrenceCountMax) {
+				curBestWord = curWord;
+				curOccurrenceCountMax = curOccurrenceCountSum;
+			}
 		}
+		
+		// Print results
+		System.out.println(curBestWord + ": " + curOccurrenceCountMax);
 	}
 
 }

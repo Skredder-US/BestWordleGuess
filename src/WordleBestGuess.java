79ba65ba029bdfe007dfcc858eeb1f6970d8367d
Taylor Juve <taylor_juve@outlook.com>
2022-04-02 13:43:04 -0700

    Just messing around at this point
    Iterative solution would take WAY too long anyways

--- src/WordleBestGuess.java
+++ src/WordleBestGuess.java
@@ -8,9 +8,15 @@ import java.util.Scanner;
  * by Taylor Juve
  * 
  * 6741 non-plural wordle words out of 10663 total.
+ * Too many options for a recursive solution (stack overflow!)
  * 
  * Attempts a deep sum to determine the sum of all words that contain 
- * letters as the first letter (and so on.)
+ * letters as the first word (and so on.)
+ * 
+ * Guess: aahed
+ * 30405093
+ * Guess: sared
+ * 32124866
  */
 public class WordleBestGuess {
 
@@ -18,8 +24,8 @@ public class WordleBestGuess {
 		// Locally store all non-plural Wordle words
 		List<String> words = new ArrayList<String>();
 			// .txt file should stay a space separated list of Wordle words
-//		Scanner wordsScan = new Scanner(new File("src/Wordle Word List.txt"));
-		Scanner wordsScan = new Scanner(new File("src/Test Words.txt"));
+		Scanner wordsScan = new Scanner(new File("src/Wordle Word List.txt"));
+//		Scanner wordsScan = new Scanner(new File("src/Test Words.txt"));
 		while (wordsScan.hasNext()) {
 			String word = wordsScan.next();
 			if (!word.endsWith("s")) {
@@ -28,19 +34,23 @@ public class WordleBestGuess {
 		}
 
 		// Determine similarity of the first word
-		System.out.println("Words: " + words);
-		String guess = words.remove(0);
+//		System.out.println("Words: " + words);
+		int i = 0;
+		while (!words.get(i).equals("sared")) {
+			i++;
+		}
+		
+		String guess = words.remove(i);
 		System.out.println("Guess: " + guess);
-		System.out.println(count(guess, words));
+		System.out.println(count(guess, words, 0));
 		
 	}
 
 	// Counts the number of words with the same letters, words with same letters
 	// are then counted again and the total is summed together for a "how similar
 	// is this word and it's matching words score"
-	public static int count(String guess, List<String> words) {
-		System.out.println("guess = " + guess + ", words = " + words);
-		if (words.isEmpty()) {
+	public static int count(String guess, List<String> words, int depth) {
+		if (depth > 1 || words.isEmpty()) {
 			return 0;
 		}
 		
@@ -49,7 +59,7 @@ public class WordleBestGuess {
 			String word = words.get(i);
 			if (containsLetter(guess, word)) {
 				words.remove(i);
-				sum += 1 + count(word, new ArrayList<String>(words));
+				sum += 1 + count(word, new ArrayList<String>(words), depth + 1);
 				words.add(i, word);
 			} 
 		}
